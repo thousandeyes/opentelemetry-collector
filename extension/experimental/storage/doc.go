8a8656d66efba2add64c9afec5f77ead6d80a275
Serhiy Boychenko <sboychen@thousandeyes.com>
2023-06-22 11:46:15 +0100

    Revert "[doc] update documentation (#7810)"
    This reverts commit 0798ed39f2c3711e7ea84d8e90dca150a8aa5509.

--- extension/experimental/storage/doc.go
+++ extension/experimental/storage/doc.go
@@ -1,6 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Package storage implements an extension that can
-// persist state beyond the collector process.
+// Package stanzareceiver implements a receiver that can be used by the
+// Opentelemetry collector to receive logs using the stanza log agent
 package storage // import "go.opentelemetry.io/collector/extension/experimental/storage"
